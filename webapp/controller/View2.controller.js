1bd6b4ae8274c45d86a887ebb73872c0c3bb4aa0
RAJASEKHARAREDDY GORAKHA <rajasekharareddy0195@gmail.com>
2025-02-28 02:46:04 +0000

    second time push check

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -1,31 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "jnj/rsc/workorder/project3/util/formatter"
-], (Controller, formatter) => {
+    "jnj/rsc/workorder/project3/util/formatter" // Adjust path with forward slashes
+], function (Controller, formatter) {
     "use strict";
 
     return Controller.extend("jnj.rsc.workorder.project3.controller.View2", {
         formatter: formatter,
-        onInit() {
-            this.getView().setModel(new sap.ui.model.json.JSONModel(), "formatter");
-            this.getView().getModel("formatter").setProperty("/", formatter);
 
+        onInit: function () {
+            console.log("Formatter Loaded: ", formatter);
         }
     });
 });
-
-
-// sap.ui.define([
-//     "sap/ui/core/mvc/Controller",
-//     "jnj/rsc/workorder/project3/util/formatter" // Adjust path with forward slashes
-// ], function (Controller, formatter) {
-//     "use strict";
-
-//     return Controller.extend("jnj.rsc.workorder.project3.controller.View2", {
-//         formatter: formatter,
-
-//         onInit: function () {
-//             console.log("Formatter Loaded: ", formatter);
-//         }
-//     });
-// });
